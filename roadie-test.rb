a63cd6721765fd64f2f0fa22f88c230fc61fd744
Yosuke Adachi <front.lightstone@gmail.com>
2015-06-02 23:58:22 +0100

    check different url for hover

--- roadie-test.rb
+++ roadie-test.rb
@@ -1,12 +1,12 @@
 require 'net/http'
 require 'roadie'
 
-uri = URI('https://blog.pusher.com/websockets-from-scratch/?utm_source=webopsweekly&utm_medium=email')
+uri = URI('https://gist.github.com/staltz/868e7e9bc2a7b8c1f754#file-introrx-md')
 html = Net::HTTP.get(uri)
 
 document = Roadie::Document.new html
 
-File.open("output2.html", "w") do |fout|
+File.open("output3.html", "w") do |fout|
   fout.puts document.transform
 end
 
