0aabe39f6c87ebc54bc605dc16a9f82b8e1c99ab
Yosuke Adachi <front.lightstone@gmail.com>
2015-06-04 23:28:40 +0100

    testing custome white list

--- sanitize-html.rb
+++ sanitize-html.rb
@@ -1,20 +1,21 @@
+require 'nokogiri'
 require 'sanitize'
 
 
-html = File.open('/Users/adachi/Documents/Kaihatsu/inlinecss-test/extract4.html')
-@striped_html = Sanitize.fragment(html,
-  :elements => ['a', 'span'],
+f = File.open('/Users/adachi/Documents/Kaihatsu/inlinecss-test/extract4.html')
+@html = Nokogiri::HTML(f)
+f.close
 
+@stripped_html = Sanitize.fragment(@html,
+  :elements => ['a', 'abbr', 'acronym', 'address', 'area', 'b', 'bdo', 'big', 'blockquote', 'br', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'font', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'ins', 'kbd', 'li', 'map', 'ol', 'p', 'pre', 'q', 's', 'samp', 'small', 'span', 'strike', 'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'title', 'tr', 'tt', 'u', 'ul', 'var', 'xmp'],
   :attributes => {
-    'a'    => ['href', 'title'],
+    'body' => ['style'],
+    'a'    => ['href', 'title', 'style'],
     'span' => ['class']
-  },
-
-  :protocols => {
-    'a' => {'href' => ['http', 'https', 'mailto']}
   }
 )
-File.open('striped.html', 'w') do |fout|
-  puts @stripped_html
+
+File.open('striped2.html', "w") do |fout|
+  p fout.puts @stripped_html
 end
-html.close
+
