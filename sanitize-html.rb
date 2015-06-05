bd933fe83952af432c7452997b786e1fa89d560e
Yosuke Adachi <front.lightstone@gmail.com>
2015-06-05 07:48:25 +0100

    use custome white list on sanitize

--- sanitize-html.rb
+++ sanitize-html.rb
@@ -6,12 +6,10 @@ f = File.open('/Users/adachi/Documents/Kaihatsu/inlinecss-test/extract4.html')
 @html = Nokogiri::HTML(f)
 f.close
 
-@stripped_html = Sanitize.fragment(@html,
-  :elements => ['a', 'abbr', 'acronym', 'address', 'area', 'b', 'bdo', 'big', 'blockquote', 'br', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'font', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'ins', 'kbd', 'li', 'map', 'ol', 'p', 'pre', 'q', 's', 'samp', 'small', 'span', 'strike', 'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'title', 'tr', 'tt', 'u', 'ul', 'var', 'xmp'],
+@stripped_html = Sanitize.document(@html,
+  :elements => ['html', 'body', 'a', 'abbr', 'acronym', 'address', 'area', 'b', 'bdo', 'big', 'blockquote', 'br', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'div', 'dl', 'dt', 'em', 'font', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'ins', 'kbd', 'li', 'map', 'ol', 'p', 'pre', 'q', 's', 'samp', 'small', 'span', 'strike', 'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'title', 'tr', 'tt', 'u', 'ul', 'var', 'xmp'],
   :attributes => {
-    'body' => ['style'],
-    'a'    => ['href', 'title', 'style'],
-    'span' => ['class']
+    :all    => ['href', 'title', 'style', 'id', 'class']
   }
 )
 
